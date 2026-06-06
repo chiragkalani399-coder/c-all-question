26449978fface5da92148b26a285f228380dac1a
chiragkalani399-coder <chiragkalani399@gmail.com>
2026-06-06 09:31:49 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,11 @@
+// area of the circle
+#include <stdio.h>
+int main()
+{
+    float r;
+    printf("enter the radius of the circle: ");
+    scanf("%f", &r);
+
+    printf("area of the circle is %f", 3.14 * r * r);
+    return 0;
+}
