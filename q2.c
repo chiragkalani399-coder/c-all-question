3657de3bbd49a76820b5e51f08f3f9bb22f99053
chiragkalani399-coder <chiragkalani399@gmail.com>
2026-06-06 09:30:13 +0530

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,10 @@
+// Area of the square
+#include <stdio.h>
+int main()
+{
+    int s;
+    printf("Enter the side of the square: ");
+    scanf("%d", &s);
+    printf("Area of the square is %d", s * s);
+    return 0;
+}
