519cfe21c7b352d1a73b64d25f78d2511fb2a8d6
chiragkalani399-coder <chiragkalani399@gmail.com>
2026-06-06 11:15:01 +0530

    Add files via upload

---
+++ div_by_2.c
@@ -0,0 +1,17 @@
+// to check if a number is divisible by 2 or not
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    if (n % 2 == 0)
+    {
+        printf("%d is divisible by 2", n);
+    }
+    else
+    {
+        printf("%d is not divisible by 2", n);
+    }
+    return 0;
+}
