9f9796d31867f00d2d81f3cc0a82acf12353cc5b
chiragkalani399-coder <chiragkalani399@gmail.com>
2026-06-06 09:44:56 +0530

    Add files via upload

---
+++ cube_of_number.c
@@ -0,0 +1,10 @@
+// to find the cube of a number
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    printf("the cube of the number is %d", n * n * n);
+    return 0;
+}
